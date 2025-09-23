c9972d2a7cf0aaf90366e3de49e0e666f74f33c4
Jasurbek Haydaraliyev <129217406+haydaraliyevjasurbek@users.noreply.github.com>
2025-09-23 09:51:55 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,46 +10,51 @@ function App() {
   const [words, setWords] = useState([]);
   const [step, setStep] = useState("form");
   const [score, setScore] = useState(0);
-  const [time, setTime] = useState(3);
+  const [quizTime, setQuizTime] = useState(3);
   const [showAbout, setShowAbout] = useState(
     () => !localStorage.getItem("sw_about_seen")
   );
 
+  const handleStartQuiz = (list, time) => {
+    setWords(list);
+    setQuizTime(time);
+    setStep("quiz");
+  };
+
+  const handleFinishQuiz = (correctCount) => {
+    setScore(correctCount);
+    setStep("result");
+  };
+
+  const handleRestart = () => {
+    setWords([]);
+    setScore(0);
+    setStep("form");
+  };
+
+  const handleCloseAbout = () => {
+    localStorage.setItem("sw_about_seen", "1");
+    setShowAbout(false);
+  };
+
   return (
     <div className="app">
       <div className="app__container">
-        {step === "form" && (
-          <WordForm
-            onSubmit={(list, time) => {
-              setWords(list);
-              setTime(time);
-              setStep("quiz");
-            }}
-          />
-        )}
+        {step === "form" && <WordForm onSubmit={handleStartQuiz} />}
 
         {step === "quiz" && (
-          <Quiz
-            words={words}
-            time={time}
-            onFinish={(correctCount) => {
-              setScore(correctCount);
-              setStep("result");
-            }}
-          />
+          <Quiz words={words} time={quizTime} onFinish={handleFinishQuiz} />
         )}
 
         {step === "result" && (
-          <Result
-            score={score}
-            total={words.length}
-            onRestart={() => setStep("form")}
-          />
+          <Result score={score} total={words.length} onRestart={handleRestart} />
         )}
       </div>
 
-      {showAbout && <AboutModal onClose={() => setShowAbout(false)} />}
-      <AdBanner />
+      {showAbout && <AboutModal onClose={handleCloseAbout} />}
+
+      {/* Reklama faqat result sahifasida ko‘rsatilsin */}
+      {step === "result" && <AdBanner />}
     </div>
   );
 }
